ab5bf6f5e8979d1c9c1cf2ed882a603be7612ceb
Marie465 <papsdorfmarie67@gmail.com>
2025-11-06 23:21:07 +0100

    Update Binaer.java

--- Programmierpraktikum/Übung/01/Binaer.java
+++ Programmierpraktikum/Übung/01/Binaer.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-
+/**
+ * Binaer
+ * Konvertiert eine als String eingegebene binäre Zahl in ihre entsprechende
+ * Dezimalzahl. Die Klasse prüft die Eingabe auf Gültigkeit (nur '0' und '1')
+ * und verwendet die Potenzreihe zur Berechnung der Dezimaldarstellung.
+ *
+ * @author Marie Papsdorf
+ * @version 1.0
+ * @since 2025-11-06
+ */
 public class Binaer {
     public static void main(String[] args) {
 
-        //initieierung aller benötigter variablen
-        String input;
-        int i = 0;
-        double num=0;
-        double ergebniss=0;
-        double dezimal = 0;
-        boolean yn = true;
+    	String input; // Die eingegebene Binärzahl als String
+        int i = 0; // Zählvariable/Index für die Schleife
+        double num = 0; // Temporäre Variable zur Speicherung des aktuellen Bits (0 oder 1)
+        double ergebniss = 0; // Die kumulierte Dezimalzahl (Endergebnis)
+        double dezimal = 0; // Der Dezimalwert des aktuell verarbeiteten Bits
+        boolean yn = true; // Flag zur Gültigkeitsprüfung der Eingabe (true, solange nur 0/1 enthalten sind)
 
-        //Scanner und arrays werden definiert
         Scanner scn = new Scanner(System.in);
-        ArrayList<Double> ez = new ArrayList<>();
-        ez.add(1.0);ez.add(0.0);
+        
+        // Liste zur einfachen Überprüfung, ob der aktuelle Wert 1.0 oder 0.0 ist
+        ArrayList<Double> binaer = new ArrayList<>();
+        binaer.add(1.0);binaer.add(0.0);
         
         
         System.out.println("Gib eine Binaerzahl ein");
@@ -30,7 +39,7 @@ public class Binaer {
             num=input.charAt(i)-'0';
             
             //ueberprueft ob der input wirklich nur 0 oder 1 enthaelt
-            if (!ez.contains(num)) {
+            if (!binaer.contains(num)) {
                 yn = false;
                 System.out.println("bitte gib nur null oder eins ein");
                 break;
@@ -43,7 +52,7 @@ public class Binaer {
             i++;
         }
         
-        //gibt nur das ergebinissaus wenn es sich um eine binaere zahl handelt
+        //gibt nur das ergebiniss aus wenn es sich um eine binaere zahl handelt
         if (yn==true){
             System.out.println(input+" in binärschreibweise ist " + (int) ergebniss +" in dezimalschreibweise");
         }
