f6e55728e093628517352c9326b86a0ca6cbea83
Marie465 <papsdorfmarie67@gmail.com>
2025-11-06 20:35:58 +0100

    Add Zeichenkette program to process sentences
    This program takes a sentence input, removes vowels, and prints the result. It then processes a second sentence, displaying parts of it iteratively.

---
+++ Programmierpraktikum/Übung/01/Zeichenkette.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Zeichenkette {
+    public static void main(String[] args) {
+
+        String satz;
+        String satz1;
+        int length;
+        int i = 0;
+        int a = 1;
+        String ergebniss = "";
+
+        ArrayList Vokale = new ArrayList();
+        ArrayList Satz = new ArrayList();
+        Scanner scn = new Scanner(System.in);
+
+        Vokale.add("a");
+        Vokale.add("e");
+        Vokale.add("i");
+        Vokale.add("o");
+        Vokale.add("u");
+
+        System.out.println("Gib einen satz ein:");
+        System.out.println("");
+        satz = scn.nextLine();
+
+        while (i < satz.length()) {
+            if (!Vokale.contains(("" + satz.charAt(i)).toLowerCase())) {
+                Satz.add("" + satz.charAt(i));
+            }
+            i++;
+        }
+
+        i = 0;
+
+        while (i < Satz.size()) {
+            ergebniss += "" + Satz.get(i);
+            i++;
+        }
+        System.out.println(ergebniss);
+        System.out.println("");
+        System.out.println("Gib einen weiteren satz ein:");
+        System.out.println("");
+        satz1 = scn.nextLine();
+
+        i = 0;
+
+        boolean ende = false;
+
+        Satz.removeAll(Satz);
+
+        ergebniss = "";
+
+        while (ende != true) {
+
+            if (satz1.length() > 1) {
+
+                while (i < Math.round((satz1.length() / 2.0))) {
+                    Satz.add("" + satz1.charAt(i));
+                    ergebniss += "" + Satz.get(i);
+                    i++;
+                }
+
+                Satz.removeAll(Satz);
+                System.out.println(a + "." + ergebniss);
+                i = 0;
+                satz1 = ergebniss;
+                ergebniss = "";
+                a++;
+            } else {
+                ende = true;
+            }
+        }
+        scn.close();
+    }
+
+}
